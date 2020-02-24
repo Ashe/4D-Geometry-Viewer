1a62e8584a0203c5f89360c369ebb6dab135131d
Ashley Smith <ashley@aas.sh>
2020-02-24 14:08:29 +0000

    Centered cube around ((00,,  00,,  00))

--- src/Tesseract.cpp
+++ src/Tesseract.cpp
@@ -16,9 +16,9 @@ App::Tesseract::Tesseract() {
 
   // Create a unit tesseract
   for (int w = 0; w < 2; ++w) {
-    for (int z = 0; z < 2; ++z) {
-      for (int y = 0; y < 2; ++y) {
-        for (int x = 0; x < 2; ++x) {
+    for (int z = -1; z < 2; z += 2) {
+      for (int y = -1; y < 2; y += 2) {
+        for (int x = -1; x < 2; x += 2) {
 
           // x, y, z, w
           vertices[vertexCount++] = (float)x;
